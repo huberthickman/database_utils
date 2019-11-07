3a3d9abb4a3f7cc8ab70eccd50a57b47da43348b
Hubert Hickman <hickmanhb@nih.gov>
2019-11-06 21:56:39 -0600

    Initial commit of utility to import a sheet from an Excel workbook into a sqlite table.

---
+++ import_tables.py
@@ -0,0 +1,32 @@
+#
+# Import a sheet from an Excel workbook page into a sqlite database.
+# The table name is the sheet name.
+#
+import sqlite3
+
+import argparse
+
+import pandas
+
+
+parser = argparse.ArgumentParser(description='Import a page in an Excel workbook into a sqlite databases')
+
+parser.add_argument('--dbfilename', action='store', type=str, required=True)
+parser.add_argument('--excel_file_name', action='store', type=str, required=True)
+parser.add_argument('--sheetname', action='store', type=str, required=True)
+
+args = parser.parse_args()
+
+con = sqlite3.connect(args.dbfilename)
+cur = con.cursor()
+
+df = pandas.read_excel(args.excel_file_name, sheetName=[args.sheetname])
+df.columns = [c.replace(' ', '_').lower() for c in df.columns]
+
+
+table_name = args.sheetname.lower().replace('-', '_').replace(' ', '_')
+print('Table name is ' + table_name)
+
+df.to_sql(table_name, con=con, if_exists='replace')
+
+con.close()
